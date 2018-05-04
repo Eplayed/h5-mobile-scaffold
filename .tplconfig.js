f9c2bf4952fccf0987493a25620316184c782ec5
cntanglijun <869058216@qq.com>
2018-05-04 22:56:19 +0800

    添加 .tplconfig.js 字段说明

--- .tplconfig.js
+++ .tplconfig.js
@@ -1,10 +1,22 @@
 module.exports = {
+
+  // 资源目录
   srcRoot: 'src',
+
+  // 发布目录
   distRoot: 'dist',
+
+  // 需要 babel 解析的文件后缀名
   jsExtName: 'es',
+
+  // 需要 pug 解析的文件后缀名
   htmlExtName: 'pug',
+
+  // 需要 styl 解析的文件后缀名
   cssExtName: 'styl',
-  ignoreDirs: [
+
+  // 发布需要忽略的目录
+  distIgnoreDirs: [
     'template'
   ]
 }
